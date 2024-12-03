b0f0f58fc08d274ed333e3a077bca10e1ebcdc1b
Nolan Porter <porternolan62@gmail.com>
2024-12-03 01:36:49 -0500

    Day 3 finished

---
+++ Day3/README.md
@@ -0,0 +1,16 @@
+## Thoughts
+
+I hate regex, and also the wording on the second question was abiguous and led me down a rabbit hole
+wasting like 10 minutes.
+
+## Total time spent
+~20 minutes
+
+### Part 1
+#### Time spent: 5 minutes
+This was pretty straight forward regex matching.
+
+
+### Part 2
+#### Time spent: 15 minutes
+This was also pretty straight forward regex matching. Got tripped up on some wording about the muls being valid at the beginning of the program. I thought that each line represented a program so I was resetting the mul to true on each line, rather than just at the very beginning. Oops...
--- Day3/main.py
+++ Day3/main.py
@@ -1,27 +1,41 @@
 import re
 
+pattern_mul = r"mul\((\d+),(\d+)\)"
+pattern_program = r"mul\(\d+,\d+\)|do\(\)|don['’]t\(\)"
+pattern_nums = r"\d+"
+
+'''
+Simple regex pattern match to get all of the mul instructions, then iterate over them
+'''
 def part1() -> int:
     val = 0
-    pattern = r"mul\((\d+),(\d+)\)"
     with open('input.txt') as f:
         for line in f.readlines():
-            matches = re.findall(pattern, line)
-            numbers = [(int(a), int(b)) for a, b in matches]
+            numbers = [(int(a), int(b)) for a, b in re.findall(pattern_mul, line)]
+            print(numbers)
             for pair in numbers:
                 val += pair[0]*pair[1]
 
     return val
 
 '''
-NOT DONE YET
+Another simple regex pattern match with a changing boolean based on instruction.
 '''
 def part2() -> int:
     val = 0
     with open('input.txt') as f:
+        do_mult = True
         for line in f.readlines():
-            val += 1
+            for i in re.findall(pattern_program, line):
+                if i == "do()":
+                    do_mult = True
+                elif i == "don't()":
+                    do_mult = False
+                else:
+                    if do_mult:
+                        pair1, pair2 = [int(num) for num in re.findall(pattern_nums, i)]
+                        val += pair1*pair2
     return val
-
 
 print(part1())
 print(part2())
